6e54c341c5e51ce5441ff4dfc816087fc3660315
Aryan Gupta <aryangtp@gmail.com>
2026-01-30 16:09:21 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ await connectCloudinary()
 ======================= */
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5017'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5017', 'https://lms-project-five-theta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   })
 )

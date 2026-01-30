15409c348bb6f42a5f7ffab7c4cd747a6eba4011
Aryan Gupta <aryangtp@gmail.com>
2026-01-30 15:14:38 +0530

    Add vercel config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import app from './server.js'
-
 const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+// Only listen if NOT running on Vercel
+if (process.env.VAMPIRE_VARIABLE !== 'true') { // Just check an env var or if require.main
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+}
+export default app
